253c1fa9fae5750a24acd9ba88fc832691e2b6bd
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-23 22:37:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,5 @@ model = load('Colon_cancer_svc.joblib')
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-@app.post("/uploadcsv")
-async def upload_file(file: UploadFile):
-    dataframe = pd.read_csv(file.file)
-    prediction = model.predict(dataframe).tolist()
-    result=pd.DataFrame(prediction)
-    result=result.replace({0:'normal',1:'adenocarcinoma'})
-    result=result.values.tolist()
-    return {"prediction": result,}    
+    return {"message": "Hello World"}    
     
