7d995a65b29973b01edd46a25d426250343509b2
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-23 22:01:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 
 from fastapi import FastAPI
 
-app = FastAPI()
-
-
+app = FastAPI(title="Colon ML API", description="Colon for iris dataset ml model", version="1.0",)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-   
