54e67fdf5468704ef095d21a98d65c0b0a052919
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-23 20:48:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
+from fastapi import FastAPI, Body,UploadFile,Header,File
+import pandas as pd
+from joblib import load
 
-from fastapi import FastAPI
-
+Model = load('Colon_cancer_svc.joblib')
 
 
 app = FastAPI()
@@ -13,6 +15,19 @@ def home():
     return {"message":"Hello TutLinks.com"}
 
 
+@app.on_event('startup')
+def load_model():
+    global model
+    model = Model
 
 
 
+@app.post("/uploadcsv")
+async def upload_file(file: UploadFile):
+    dataframe = pd.read_csv(file.file)
+    prediction = model.predict(dataframe).tolist()
+    result=pd.DataFrame(prediction)
+    result=result.replace({0:'normal',1:'adenocarcinoma'})
+    result=result.values.tolist()
+    return {"prediction": result,}    
+    
