e16491f364218f0307347ff2674e6d19349b7308
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-23 22:53:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from joblib import load
 import pandas as pd
 
 app = FastAPI()
-model = load('Colon_cancer_svc.joblib')
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}    
     
-
+@app.on_event('startup')
+def load_model():
+    global model
+    model = load('Colon_cancer_svc.joblib')
+    
+    
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     return {"filename": file.filename}
