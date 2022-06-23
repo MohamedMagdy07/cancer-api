78e8deb1d768d37aac43a2e7dde963dfdbd6102d
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-24 00:17:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 from fastapi import FastAPI, Body,UploadFile,Header,File
 from joblib import load
 import pandas as pd
+import sys
 
 app = FastAPI()
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    print(base_path)
+    return os.path.join(base_path, relative_path)
 
 
 @app.post("/")
 async def root():
-    model = load('1243.joblib')    
+    model = load(resource_path('Colon_cancer_svc.joblib'))    
     return {"message": 'hello'}    
     
 #@app.on_event('startup')
