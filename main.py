f496875d02114c3cf2e88445e7618b45360df314
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-24 01:01:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ async def root():
 async def upload_file(file: UploadFile):
     data = pd.read_csv(file.file, header=None)
     if len(data.columns) == 50:
-        prediction = model.predict(data)
-        result = pd.DataFrame(prediction)
-        result = result.replace({0: 'Normal', 1: 'Tumoral '})
-        print(result)
+        prediction = model.predict(data).tolist()
+        result=pd.DataFrame(prediction)
+        result=result.replace({0:'normal',1:'tomural'})
+        result=result.values.tolist()
     else:
         result = "Please Upload Usable Data File"
     return {"prediction": result,}   
