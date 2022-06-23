83919bafbde693fcfb55e72ac2b31bcbb68f4ec4
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-23 20:43:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 
-from pydantic import BaseModel, conlist
-import pandas as pd
+
+from fastapi import FastAPI
 
 
 
-from fastapi import FastAPI, Body,UploadFile,Header,File
-from joblib import load
-
-Model = load('Colon_cancer_svc.joblib')
-
-
-
-
-from fastapi import FastAPI, Body,UploadFile,Header,File
-from joblib import load
-
-
-app = FastAPI(title="Colon ML API", description="Colon for iris dataset ml model", version="1.0",)
+app = FastAPI()
 
 
 
@@ -26,20 +14,5 @@ def home():
 
 
 
-@app.on_event('startup')
-def load_model():
-    global model
-    model = Model
 
-
-
-@app.post("/uploadcsv")
-async def upload_file(file: UploadFile):
-    dataframe = pd.read_csv(file.file)
-    prediction = model.predict(dataframe).tolist()
-    result=pd.DataFrame(prediction)
-    result=result.replace({0:'normal',1:'adenocarcinoma'})
-    result=result.values.tolist()
-    return {"prediction": result,}    
-    
 
