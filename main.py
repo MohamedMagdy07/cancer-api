c08f171844a33e2337b5db7b30c2f7902b3dc6c0
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-23 23:14:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    model = load('Colon_cancer_svc.joblib')
+    model = load('./Colon_cancer_svc.joblib')
     return {"message": model}    
     
     
