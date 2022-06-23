31feabab5f2bb8e8250a62ccaf2cb13b44232bc6
Mohamed Magdy <79805311+MohamedMagdy07@users.noreply.github.com>
2022-06-23 22:24:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body,UploadFile,Header,File
+from joblib import load
+import pandas as pd
 
 app = FastAPI()
+
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+@app.on_event('startup')
+def load_model():
+    global model
+    model = load('Colon_cancer_svc.joblib')
+
+
+
+@app.post("/uploadcsv")
+async def upload_file(file: UploadFile):
+    dataframe = pd.read_csv(file.file)
+    prediction = model.predict(dataframe).tolist()
+    result=pd.DataFrame(prediction)
+    result=result.replace({0:'normal',1:'adenocarcinoma'})
+    result=result.values.tolist()
+    return {"prediction": result,}    
+    
